03a1cf90ef65b6e0b33bd0704b5577b709d16a01
Sandra Opare <ewuramaaddo@gmail.com>
2023-11-06 18:35:59 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+     <h1>Hello World</h1>
+     <p>I am Sandra Opare-Twum,This is my first React Application.</p>
+     
+    </>
+  )
+}
+
+export default App
